735e827cae4f131ef1955e2671b253e4f8f27af1
Shreenath Tewari <stewari.sps@gmail.com>
2019-01-09 11:10:15 +0530

    jwt_authenticatable changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :jwt_authenticable,
+         :jwt_authenticatable,
          jwt_revocation_strategy: JWTBlacklist
 end
